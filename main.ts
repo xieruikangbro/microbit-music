1739f7356648eafd469da9129ea719f08de16043
xieruikangbro <73052339+xieruikangbro@users.noreply.github.com>
2021-02-14 13:07:53 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,191 @@
+music.playTone(330, music.beat(BeatFraction.Half))
+music.playTone(392, music.beat(BeatFraction.Half))
+music.playTone(523, music.beat(BeatFraction.Half))
+music.playTone(659, music.beat(BeatFraction.Half))
+music.playTone(587, music.beat(BeatFraction.Half))
+music.playTone(523, music.beat(BeatFraction.Half))
+music.playTone(587, music.beat(BeatFraction.Half))
+music.playTone(523, music.beat(BeatFraction.Half))
+music.playTone(494, music.beat(BeatFraction.Half))
+music.playTone(440, music.beat(BeatFraction.Half))
+music.playTone(494, music.beat(BeatFraction.Half))
+music.playTone(262, music.beat(BeatFraction.Half))
+music.playTone(523, music.beat(BeatFraction.Half))
+music.playTone(440, music.beat(BeatFraction.Double))
+music.playTone(330, music.beat(BeatFraction.Half))
+music.playTone(440, music.beat(BeatFraction.Half))
+music.playTone(523, music.beat(BeatFraction.Half))
+music.playTone(659, music.beat(BeatFraction.Half))
+music.playTone(587, music.beat(BeatFraction.Half))
+music.playTone(523, music.beat(BeatFraction.Half))
+music.playTone(659, music.beat(BeatFraction.Half))
+music.playTone(587, music.beat(BeatFraction.Half))
+music.playTone(523, music.beat(BeatFraction.Half))
+music.playTone(587, music.beat(BeatFraction.Half))
+music.playTone(523, music.beat(BeatFraction.Half))
+music.playTone(494, music.beat(BeatFraction.Half))
+music.playTone(440, music.beat(BeatFraction.Half))
+music.playTone(494, music.beat(BeatFraction.Half))
+music.playTone(523, music.beat(BeatFraction.Double))
+music.playTone(440, music.beat(BeatFraction.Half))
+music.playTone(523, music.beat(BeatFraction.Half))
+music.playTone(659, music.beat(BeatFraction.Half))
+music.playTone(784, music.beat(BeatFraction.Half))
+music.playTone(698, music.beat(BeatFraction.Whole))
+music.playTone(392, music.beat(BeatFraction.Half))
+music.playTone(494, music.beat(BeatFraction.Half))
+music.playTone(587, music.beat(BeatFraction.Half))
+music.playTone(698, music.beat(BeatFraction.Half))
+music.playTone(659, music.beat(BeatFraction.Whole))
+music.playTone(330, music.beat(BeatFraction.Half))
+music.playTone(440, music.beat(BeatFraction.Half))
+music.playTone(523, music.beat(BeatFraction.Half))
+music.playTone(659, music.beat(BeatFraction.Half))
+music.playTone(587, music.beat(BeatFraction.Half))
+music.playTone(587, music.beat(BeatFraction.Half))
+music.playTone(659, music.beat(BeatFraction.Half))
+music.playTone(587, music.beat(BeatFraction.Half))
+music.playTone(523, music.beat(BeatFraction.Half))
+music.playTone(494, music.beat(BeatFraction.Half))
+music.playTone(440, music.beat(BeatFraction.Half))
+music.playTone(494, music.beat(BeatFraction.Whole))
+music.playTone(330, music.beat(BeatFraction.Half))
+music.playTone(440, music.beat(BeatFraction.Half))
+music.playTone(523, music.beat(BeatFraction.Half))
+music.playTone(659, music.beat(BeatFraction.Half))
+music.playTone(587, music.beat(BeatFraction.Half))
+music.playTone(587, music.beat(BeatFraction.Half))
+music.playTone(659, music.beat(BeatFraction.Half))
+music.playTone(587, music.beat(BeatFraction.Half))
+music.playTone(523, music.beat(BeatFraction.Half))
+music.playTone(494, music.beat(BeatFraction.Half))
+music.playTone(523, music.beat(BeatFraction.Half))
+music.playTone(440, music.beat(BeatFraction.Double))
+basic.forever(function () {
+    basic.showLeds(`
+        . # . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        # . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        # . . . .
+        # . . . .
+        . # . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        # . . . .
+        # . . . .
+        . # . . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        # . . . .
+        # . . . .
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        # . . . .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        # . . . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # # # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # # # . #
+        # # . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # # # . #
+        # # . . #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # # # . #
+        # # . # #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # . # #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        # . # # .
+        . . # . #
+        . # # . .
+        `)
+    basic.showLeds(`
+        # # # # .
+        # . . # .
+        # # . # .
+        # . # # .
+        # . . . #
+        `)
+})
